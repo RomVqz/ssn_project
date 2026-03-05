915493fc1f2c20e0371c993997d913bdd96c4336
DvdRivas <davidadalrivasavila@gmail.com>
2026-03-05 11:55:30 -0600

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,40 +10,34 @@ apt-get update && \
       pkg-config \
       libssl-dev \
       sudo \
-      tini &&
+      tini
 
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
 
 curl --proto '=https' --tlsv1.2 -sSfL https://solana-install.solana.workers.dev | bash
 
-echo 'export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 
-echo "--- Instalación de Rust completada ---"
+echo "--- Instalación de Rust, Solana y Anchor completada ---"
 
-sudo apt update
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash    
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm install node 
-
+nvm install node
 nvm use node
 
-npm install -g typescript   
-
-npm install @solana/web3.js @coral-xyz/anchor
+npm install -g typescript
 
 anchor init template_codespaces
 
 cd template_codespaces
 
+npm install @solana/web3.js @coral-xyz/anchor
+
 solana config set --url https://api.devnet.solana.com
 
-solana-keygen new --no-bip39-passphrase --outfile ~/.config/solana/id.json 
+solana-keygen new --no-bip39-passphrase --outfile ~/.config/solana/id.json
 
 while true; do
   echo "¿Usaras codespaces para desarrollar en Solana? (no = solo lo usare para el frontend) (si/no)"
@@ -52,10 +46,10 @@ while true; do
   if [ "$respuesta" = "si" ]; then
     anchor build
     break
-  elif
+  elif [ "$respuesta" = "no" ]; then
     echo "Entorno listo para usar!!! :D"
     break
-  else echo "Opción no válida. Por favor, ingresa 'si' o 'no'."
+  else
+    echo "Opción no válida. Por favor, ingresa 'si' o 'no'."
   fi
-done 
-
+done
