6dcb5644aaa14d4723f8bb7177e77f7e656975a5
DvdRivas <davidadalrivasavila@gmail.com>
2026-03-05 11:06:40 -0600

    .

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -25,6 +25,8 @@ sudo apt update
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash    
 
+export NVM_DIR="$HOME/.nvm"
+
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install node 
